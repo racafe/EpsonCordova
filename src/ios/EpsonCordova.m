10e32ec595ea4134e8e84a16f413c3b8d5b3304e
Rafael Carrion <rafael.carrion@technit.com.mx>
2017-07-14 14:13:23 -0500

    método de imprimir

--- src/ios/EpsonCordova.m
+++ src/ios/EpsonCordova.m
@@ -14,7 +14,7 @@
 	printer = [[Epos2Printer alloc] initWithPrinterSeries:EPOS2_TM_T20 lang:EPOS2_MODEL_ANK]; 
 	if (printer == nil) { 
 		//pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Problema con impresora"];
+		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Problema con impresora"];
 	}else{
 		[printer setReceiveEventDelegate:self];
 		int result = EPOS2_SUCCESS; 
@@ -22,30 +22,49 @@
 		result = [printer addText:@"Hello World"]; 
 		if (result != EPOS2_SUCCESS) { 
 			//Displays error messages 
-			//pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-			pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Problema con agregar texto"];
+			pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Problema con agregar texto"];
 		}else{
 			result = [printer connect:[@"TCP:" stringByAppendingString:ip] timeout:EPOS2_PARAM_DEFAULT]; 
 			result = [printer beginTransaction]; 
 			if (result != EPOS2_SUCCESS) { 
 				//Displays error messages 
 				pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Problema con conexión a ip"];
-				//pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Problema con conexión a ip"];
 			}else{
 				Epos2PrinterStatusInfo *status = nil; 
 				status = [printer getStatus]; 
 				if (status.getConnection && status.getOnline) { 
 					result = [printer sendData:EPOS2_PARAM_DEFAULT]; 
-					pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Todo bien"];
+					
+					- (void) onPtrReceive:(Epos2Printer *)printerObj code:(int)code status:(Epos2PrinterStatusInfo *)status printJobId:(NSString *)printJobId { 
+						if (code == EPOS2_CODE_SUCCESS) { 
+							//Displays successful print messages 
+							pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Todo bien"];
+						} else { 
+							//Displays error messages 
+							pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Problema al imprimir"];
+						} 
+						//Abort process 
+					}
+
+					
 				}else{ 
 					//Displays error messages 
 					//Abort process 
 					//pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-					pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Problema con obtener status"];
+					pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Problema con obtener status"];
 				} 
 			}
 		}
 	}
+	
+	int result = EPOS2_SUCCESS; 
+	result = [printer endTransaction]; 
+	result = [printer disconnect];
+	
+	[printer clearCommandBuffer]; 
+	[printer setReceiveEventDelegate:nil]; 
+	printer = nil;
+
 
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
